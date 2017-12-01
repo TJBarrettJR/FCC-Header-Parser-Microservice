3ae223480bcfb9605ff3576354c667905aa78f63
Glitch (hurricane-microwave) <none>
2017-12-01 18:14:33 +0000

    Completed Project

--- server.js
+++ server.js
@@ -5,8 +5,11 @@
 var express = require('express');
 var app = express();
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+app.enable('trust proxy');
+
+app.get('/whoami', function(req, res) {
+  res.status(200).json({ "ipaddress": req.ip,"language": req.acceptsLanguages()[0],"software": req.get('user-agent').match(/\(([^\)]+)\)/)[1] })
+});
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -15,23 +18,6 @@ app.use(express.static('public'));
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
-
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
-});
-
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
-});
-
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
